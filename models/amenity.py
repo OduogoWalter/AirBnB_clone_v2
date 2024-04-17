2f7c5f25619126560e2359a5828f395b74dec272
OduogoWalter <oduogowalter@gmail.com>
2024-04-17 09:30:43 +0300

    models

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,21 @@
-#!/usr/bin/python3
-"""Amenity Module for HBNB project"""
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
 
-class Amenity(BaseModel):
-    """Amenity class that represents amenities offered in a place."""
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes Amenity instance."""
+        """initializes Amenity"""
         super().__init__(*args, **kwargs)
-        self.name = ""
--- models/city.py
+++ models/city.py
@@ -1,18 +1,24 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-
-import sys
-from sqlalchemy import Column, String, ForeignKey
+#!/usr/bin/python
+""" holds class City"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """This class represents a city"""
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of city"""
+        """initializes city"""
         super().__init__(*args, **kwargs)
